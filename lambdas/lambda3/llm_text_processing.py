fdc85e8b6eab26f3790a9039b5c87eb46d70dd2d
vigneshgarrapally <garrapallyvignesh8055@gmail.com>
2023-07-13 13:52:29 +0530

    Refactor llm_text_processing.py for readability
    - Concatenate system prompts into a single string if given as a list
    - Update logging statements to reflect input variables
    - Adjust input variables when calling llm_chain.predict() function to match updated parameter names

--- lambdas/lambda3/llm_text_processing.py
+++ lambdas/lambda3/llm_text_processing.py
@@ -15,12 +15,22 @@ def lambda_handler(event, context):
     logger.info(body)
     # get text from body
     input = body.get("text", "")
-    system_prompts = body.get("prompt", "")
+    system_prompts = body.get("system_prompts", [])
+    # concatenate system prompts
+    if isinstance(system_prompts, list):
+        system_prompts = "\n".join(system_prompts)
+    elif isinstance(system_prompts, str):
+        system_prompts = system_prompts
+    else:
+        system_prompts = ""
     temparature = body.get("temparature", 0)
-    logger.info("Text: " + input)
-    logger.info("Prompt: " + system_prompts)
+    logger.info("Input: " + input)
+    logger.info("System Prompts: " + system_prompts)
+    logger.info("Temparature: " + str(temparature))
     # create prompt template
-    template = "Prompt that was provided is: {system_prompts}.Text that was provided is: {input}."
+    template = (
+        "System Prompts Provided are: {system_prompts}.Input provided is: {input}."
+    )
     prompt = PromptTemplate(
         input_variables=["input", "system_prompts"],
         template=template,
@@ -28,8 +38,8 @@ def lambda_handler(event, context):
     llm = OpenAI(temperature=temparature)
     llm_chain = LLMChain(llm=llm, prompt=prompt)
     answer = llm_chain.predict(
-        text=input,
-        provided_prompt=system_prompts,
+        input=input,
+        system_prompts=system_prompts,
     )
     logger.info("Answer: " + answer)
     result = {
